20a5949dfa43a5d6bc92482cd7abd6afcef39fc6
ASKK <102436728+ASKiere@users.noreply.github.com>
2024-03-27 21:19:07 -0400

    V1 out

---
+++ kitty.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define KITTYKAT_DEFAULT() system("echo '  ／l、\n (ﾟ､ ｡ ７\n  l、ﾞ ~ヽ\n  じしf_, )ノ\n' | lolcat");
+#define KITTYKAT_COLORLESS() system("echo '  ／l、\n (ﾟ､ ｡ ７\n  l、ﾞ ~ヽ\n  じしf_, )ノ\n'");
+#define KITTYKAT_WARNING_LOLCAT() system("echo '  ／l、\n (ﾟ､ ｡ ７\n  l、ﾞ ~ヽ\n  じしf_, )ノ\nUh-oh! Your kitty can`t have colors without lolcat installed! You may disable this warning in your kittykat.ini file!'");
+#define KITTYKAT_WARNING_CONFIG() printf("MISSING OR RENAMED CONFIG FILE. PLEASE CHECK THE KITTYKAT PATH FOR 'kittykat.txt', OR REINSTALL KITTYKAT.");
+
+/*  ／l、\n (ﾟ､ ｡ ７\n  l、ﾞ ~ヽ\n  じしf_, )ノ\n*/
+
+void txtRead()
+{
+	char* home = getenv("HOME");
+	if(home == NULL)
+	{
+		printf("Kittykat is made for linux!");
+		return;
+	}
+	
+	char kittykatconfig[256];
+	snprintf(kittykatconfig, sizeof(kittykatconfig), "%s/kitty/kittykat.txt", home);
+
+	FILE* txt = fopen(kittykatconfig,"r");
+	if(txt == NULL)
+	{
+		KITTYKAT_WARNING_CONFIG();
+	}
+
+	char line[100];
+	char* config1 = "colorful-kittykat=0";
+	
+	while(fgets(line, sizeof(line), txt) != NULL)
+	{
+		if(strstr(line,config1) != NULL)
+		{
+			KITTYKAT_COLORLESS();
+			fclose(txt);
+			
+			return;
+		}
+	}
+	fclose(txt);
+	KITTYKAT_DEFAULT();
+}
+
+int main()
+{
+	FILE* lolcat = fopen("/usr/bin/lolcat", "rb");
+	if(lolcat == NULL)
+	{
+		KITTYKAT_WARNING_LOLCAT();
+	}
+	
+	else
+	{
+		fclose(lolcat);
+		txtRead();
+	}
+
+	return 0;
+}
